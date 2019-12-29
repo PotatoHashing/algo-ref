77cf7d036b29528c3c5f0e94270c5ce2279dfb07
Aldo Chow <pflachow@gmail.com>
2019-12-29 13:08:45 -0500

    Add Fenwick Tree

---
+++ data-structures/fenwick-tree/Fenwick.h
@@ -0,0 +1,36 @@
+template <typename T>
+class Fenwick {
+ private:
+  int n;
+  vector<T> v;
+
+ public:
+  Fenwick(int n) : n(n), v(n) {}
+  Fenwick(vector<T> a) : n(a.size()), v(n) {
+    for (int i = 0; i < n; i++) {
+      v[i] = a[i];
+      int j = i | (i + 1);
+      if (j < n) a[j] += a[i];
+    }
+  }
+  void update(int pos, T dif) {  // a[pos] += dif
+    for (; pos < n; pos |= pos + 1) v[pos] += dif;
+  }
+  T query(int pos) {  // sum of values in [0, pos)
+    T res = 0;
+    for (; pos > 0; pos &= pos - 1) res += v[pos - 1];
+    return res;
+  }
+  T query(int lb, int ub) {  // sum of values in [lb, rb)
+    return query(rb) - query(lb);
+  }
+  int lower_bound(T sum) {  // min pos st sum of [0, pos] >= sum
+    // Returns n if no sum is >= sum, or -1 if empty sum is.
+    if (sum <= 0) return -1;
+    int pos = 0;
+    for (int pw = 1 << 25; pw; pw >>= 1) {
+      if (pos + pw <= n && v[pos + pw - 1] < sum) pos += pw, sum -= v[pos - 1];
+    }
+    return pos;
+  }
+};
---
+++ data-structures/fenwick-tree/README.md
@@ -0,0 +1,11 @@
+## Source:
+https://github.com/kth-competitive-programming/kactl/blob/master/content/data-structures/FenwickTree.h
+
+## Tutorial:
+https://cp-algorithms.com/data_structures/fenwick.html
+
+## Problems:
+https://open.kattis.com/problems/fenwick
+
+## Optimizations Used:
+Initialization in linear time (Discussion: https://codeforces.com/blog/entry/63064)
